6685e98f3dc104a37e986cf597d57c220a37cb48
teklumezgebo <mezge002@umn.edu>
2023-06-10 17:00:47 -0500

    routes added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import PatientForm from './PatientForm';
 import ClinicainForm from './ClinicianForm';
 import NewPatient from './NewPatient';
 import ClinicForm from './ClinicForm';
+import Homepage from './Homepage';
 
 function App() {
   return (
     <div className="App">
+      <Route exact path="/">
+      <Homepage/>
+      </Route>
+      <Route path="/patients">
       <PatientForm/>
-      <br></br>
+      </Route>
+      <Route path="/newpatient">
       <NewPatient/>
-      <br></br>
+      </Route>
+      <Route path="/clinicians">
       <ClinicainForm/>
-      <br></br>
+      </Route>
+      <Route path="/clinics">
       <ClinicForm/>
+      </Route>
     </div>
   );
 }
---
+++ src/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Homepage() {
+    return(
+        <div>
+            <h1>Are you a..?</h1>
+            <br></br>
+            <h3>Provider</h3>
+            <br></br>
+            <h3>Patient</h3>
+        </div>
+    )
+}
+
+export default Homepage
