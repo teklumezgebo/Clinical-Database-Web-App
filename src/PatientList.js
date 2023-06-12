ba7be7558f9565b2b827dedb713c515f789dfbb7
teklumezgebo <mezge002@umn.edu>
2023-06-11 19:54:52 -0500

    fetch request created

--- src/PatientList.js
+++ src/PatientList.js
@@ -1,8 +1,28 @@
 import React from "react";
+import Patient from "./Patient";
+import { useEffect, useState } from "react";
+
+
 
 function PatientList() {
+    
+    const [patientList, setPatientList] = useState(null)
+
+    useEffect(() => {
+        fetch('http://localhost:9292/patients')
+        .then(res => res.json())
+        .then(list => {
+            const listOfPatients = list.map(patient => {
+                <Patient firstName={patient.first_name} lastName={patient.last_name} hypertension={patient.hypertension} diabetes={patient.diabetes}/>
+            })
+            setPatientList(listOfPatients)
+        })
+    }, [])
+
     return(
-        {}
+        <div>
+            {patientList}
+        </div>
     )
 }
 
