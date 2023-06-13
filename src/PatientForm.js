682d8987bae3ea54e969abf17f3c2086e953b2d5
teklumezgebo <mezge002@umn.edu>
2023-06-12 20:32:46 -0500

    form created with state

--- src/PatientForm.js
+++ src/PatientForm.js
@@ -3,19 +3,48 @@ import React, { useState } from "react";
 function PatientForm() {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
+    const [homeValue, setHomeValue] = useState('')
     const [bloodPressure, setBloodPressure] = useState('')
     const [bloodSugar, setBloodSugar] = useState('')
+
+    function onFirstNameChange(event) {
+        setFirstName(event.target.value)
+    }
+
+    function onLastNameChange(event) {
+        setLastName(event.target.value)
+    }
+
+    function onHomeValueChange(event) {
+        setHomeValue(event.target.value)
+    }
+
+    function onBloodPressureChange(event) {
+        if (event.target.checked === true) {
+            setBloodPressure(true)
+        } else if (event.target.checked === false) {
+            setBloodPressure(false)
+        }
+    }
+
+    function onBloodSugarChange(event) {
+        if (event.target.checked === true) {
+            setBloodSugar(true)
+        } else if (event.target.checked === false) {
+            setBloodSugar(false)
+        }
+    }
     
     
     return (
         <form>
-            <input type="text" placeholder="First Name"></input><br></br>
-            <input type="text" placeholder="Last Name"></input><br></br>
-            <input type="text" placeholder="At home reading.."></input><br></br>
-            <input type="radio" id="selector"></input>
-            <label for="selector">Blood Pressure</label><br></br>
-            <input type="radio" id="selector"></input>
-            <label for="selector">Blood Sugar</label><br></br>
+            <input type="text" placeholder="First Name" onChange={onFirstNameChange}></input><br></br>
+            <input type="text" placeholder="Last Name" onChange={onLastNameChange}></input><br></br>
+            <input type="text" placeholder="At home reading.." onChange={onHomeValueChange}></input><br></br>
+            <input type="radio" id="bp" onChange={onBloodPressureChange}></input>
+            <label for="bpr">Blood Pressure</label><br></br>
+            <input type="radio" id="bs" onChange={onBloodSugarChange}></input>
+            <label for="bs">Blood Sugar</label><br></br>
             <input type="submit"></input>
         </form>
     )
