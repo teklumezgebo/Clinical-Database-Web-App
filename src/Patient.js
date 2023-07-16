488f64de981769e45ee65dad6e00a085d36fed8e
teklumezgebo <mezge002@umn.edu>
2023-07-16 18:18:09 -0500

    final commit

--- src/Patient.js
+++ src/Patient.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Patient({ id, firstName, lastName, hypertension, diabetes, onDelete, onClick }) {
     return (
-        <div onClick={() => onClick(firstName, lastName, hypertension, diabetes)}>
-            <h4>{firstName} {lastName}</h4>
+        <div>
+            <h4 onClick={() => onClick(firstName, lastName, hypertension, diabetes)}>{firstName} {lastName}</h4>
             <p>{hypertension ? "Hypertensive" : null}</p>
             <p>{diabetes ? "Diabetic" : null}</p>
             <button onClick={() => onDelete(id)}>Delete</button>
